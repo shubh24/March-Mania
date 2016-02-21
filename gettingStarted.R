44b8dad28a7a7eb83afeb76a8e494abf5e9ec4a5
shubh24 <shubhankar24@gmail.com>
2016-02-21 15:51:48 +0530

    Initial Setup

---
+++ gettingStarted.R
@@ -0,0 +1,7 @@
+TourneySeeds <- read.csv("../TourneySeeds.csv")
+SampleSubmission <- read.csv("../SampleSubmission.csv")
+Seasons <- read.csv("../Seasons.csv")
+Teams <- read.csv("../Teams.csv")
+TourneySlots <- read.csv("../TourneySlots.csv")
+TourneyDetailedResults <- read.csv("../TourneyDetailedResults.csv")
+TourneyCompactResults <- read.csv("../TourneyCompactResults.csv")
